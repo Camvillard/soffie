a8a0a61a8c3699d9d7d2fc6caf7959340a78ed7d
Esteban Montantes <esmo.ledev@gmail.com>
2018-11-20 17:32:59 -0500

    pundit-setup-next-code-from-serge

--- app/controllers/users_books_controller.rb
+++ app/controllers/users_books_controller.rb
@@ -1,13 +1,16 @@
 class UsersBooksController < ApplicationController
   def index
+    # @users_books = policy_scope(Users_book)
   end
 
   def new
     @users_book = UsersBook.new
-    # authorize @users_book
-
+    authorize @users_book
   end
 
   def create
+    raise
+    # serge code here
+    # authorize @users_book
   end
 end
