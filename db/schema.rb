b9be95c973862b022fd003cc8abeed26634a59e4
Esteban Montantes <esmo.ledev@gmail.com>
2018-11-20 15:15:46 -0500

    add ISBN to usersbooks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_213216) do
+ActiveRecord::Schema.define(version: 2018_11_20_201304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_213216) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "isbn"
   end
 
   add_foreign_key "book_categories", "categories", column: "categories_id"
