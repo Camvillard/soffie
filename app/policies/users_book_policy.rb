037a380f88ff05348c1b5ff125809d091405fc58
camille villard <hello@camillevillard.com>
2018-11-21 10:31:58 -0500

    resolved ossue with the policy for index page of books

--- app/policies/users_book_policy.rb
+++ app/policies/users_book_policy.rb
@@ -1,12 +1,9 @@
 class UsersBookPolicy < ApplicationPolicy
-  # class Scope < Scope
-  #   def resolve
-  #     scope.all
-
-  #     # For a multi-tenant SaaS app, you may want to use:
-  #     # scope.where(user: user)
-  #   end
-  # end
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
 
   def index?
     true
