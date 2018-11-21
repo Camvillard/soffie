b359801a37bab5f8cc0a9dda9ea76c38a0016407
camille villard <hello@camillevillard.com>
2018-11-20 22:51:52 -0500

    resolve issue with the goodreads api using google books api

--- app/controllers/users_books_controller.rb
+++ app/controllers/users_books_controller.rb
@@ -47,19 +47,40 @@ class UsersBooksController < ApplicationController
   # end
 
   def create
-    url = "https://www.googleapis.com/books/v1/volumes?q=#{params[:query]}"
-    serialized = open(url).read
-    data = JSON.parse(serialized)
-    if params["query"] != ""
-      @book = UsersBook.new(title: data["items"][0]["volumeInfo"]["title"])
-    elsif params["query"] != ""
-      @book = UsersBook.new(author: data["items"][0]["volumeInfo"]["author"])
-    else
-      @book = UsersBook.new(title: data["items"][0]["volumeInfo"]["industryIdentifiers"][0]["type"])
-    end
+    data = retrieve_information_from_google_api(params[:query])
+    # raise
+    @book = UsersBook.new
     authorize @book
     @book.save
-    raise
+
+    if @book.save
+      update_the_book_with_google_infos(@book, data)
+      redirect_to users_book_path(@book)
+    else
+      render :new
+    end
+    # raise
     # 3 cas de figure : titre / auteur / isbn
+  end
+
+  private
+
+  def retrieve_information_from_google_api(search)
+    url = "https://www.googleapis.com/books/v1/volumes?q=#{search}"
+    serialized = open(url).read
+    JSON.parse(serialized)
+  end
+
+  def update_the_book_with_google_infos(book, data)
+    book.update({
+        title: data["items"][0]["volumeInfo"]["title"],
+        author: data["items"][0]["volumeInfo"]["authors"][0],
+        description: data["items"][0]["volumeInfo"]["description"],
+        num_pages: data["items"][0]["volumeInfo"]["pageCount"]
+      })
+  end
+
+  def users_book_params
+    params.require(:user_books).permit(:title, :author, :isbn, :details, :reading_time, :num_pages, :description, :image_url)
   end
 end
