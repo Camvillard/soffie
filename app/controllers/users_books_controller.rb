3ebfbd33ef3644c68d0236a759f043a19f35c88b
camille villard <hello@camillevillard.com>
2018-11-21 08:33:45 -0500

    refactor

--- app/controllers/users_books_controller.rb
+++ app/controllers/users_books_controller.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class UsersBooksController < ApplicationController
   def index
-    # @users_books = policy_scope(Users_book)
+    # @users_books = policy_scope(UserBook)
   end
 
   def show
@@ -14,41 +14,10 @@ class UsersBooksController < ApplicationController
   def new
     @user_book = UsersBook.new
     authorize @user_book
-    # raise
   end
-
-  # def create
-  #   client = Goodreads.new(
-  #     api_key: ENV['GOODREADS_API_KEY'],
-  #     api_secret: ENV['GOODREADS_API_SECRET']
-  #   )
-  #   # first request
-      # search = search_books(params[:query])
-  #   title = search.results.work.best_book[:title]
-  #   author = search.results.work.first.best_book.author.name
-  #   image = search.results.work.first.best_book.image_url
-
-  #   # second request to get specific detail
-  #   book_detail = client.book_by_title(title)
-  #   num_pages = book_detail.num_pages
-  #   description = book_detail.description
-
-  #   @book = UsersBook.new
-  #   authorize @book
-
-  #   @book = UsersBook.create!(
-  #     title: title,
-  #     author: author,
-  #     num_pages: num_pages,
-  #     description: description,
-  #     image_url: image
-  #   )
-  #   redirect_to users_book_path(@book)
-  # end
 
   def create
     data = retrieve_information_from_google_api(params[:query])
-    # raise
     @book = UsersBook.new
     authorize @book
     @book.save
@@ -59,8 +28,6 @@ class UsersBooksController < ApplicationController
     else
       render :new
     end
-    # raise
-    # 3 cas de figure : titre / auteur / isbn
   end
 
   private
@@ -72,12 +39,12 @@ class UsersBooksController < ApplicationController
   end
 
   def update_the_book_with_google_infos(book, data)
-    book.update({
-        title: data["items"][0]["volumeInfo"]["title"],
-        author: data["items"][0]["volumeInfo"]["authors"][0],
-        description: data["items"][0]["volumeInfo"]["description"],
-        num_pages: data["items"][0]["volumeInfo"]["pageCount"]
-      })
+    book.update(
+      title: data["items"][0]["volumeInfo"]["title"],
+      author: data["items"][0]["volumeInfo"]["authors"][0],
+      description: data["items"][0]["volumeInfo"]["description"],
+      num_pages: data["items"][0]["volumeInfo"]["pageCount"]
+    )
   end
 
   def users_book_params
