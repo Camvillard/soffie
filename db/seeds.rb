126ddbffab94ca650d6574cf5c259cdad3e523ce
Serge Leclerc <leclercconseil@gmail.com>
2018-11-23 16:20:21 -0500

    Typo in the seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -389,7 +389,7 @@ pride_and_prejudice = UsersBook.create(
     title: "Pride and Prejudice",
     author: "Jane Austen",
     isbn: 9781853260001,
-    image_url: "hhttp://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    image_url: "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
     num_pages: 401,
     reading_time: "32986",
     description: "Austen’s most celebrated novel tells the story of Elizabeth Bennet, a bright, lively young woman with four sisters, and a mother determined to marry them to wealthy men. At a party near the Bennets’ home in the English countryside, Elizabeth meets the wealthy, proud Fitzwilliam Darcy. Elizabeth initially finds Darcy haughty and intolerable, but circumstances continue to unite the pair. Mr. Darcy finds himself captivated by Elizabeth’s wit and candor, while her reservations about his character slowly vanish. The story is as much a social critique as it is a love story, and the prose crackles with Austen’s wry wit.",
