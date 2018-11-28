87253727eea7905abb7fd6de164516a0a4c64cdd
camille villard <hello@camillevillard.com>
2018-11-28 16:51:04 -0500

    added default image to moods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_152413) do
+ActiveRecord::Schema.define(version: 2018_11_28_214417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_152413) do
   create_table "moods", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "image_url"
+    t.string "image_url", default: "https://res.cloudinary.com/camvillard/image/upload/v1543358544/soffie/book_1.jpg", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
